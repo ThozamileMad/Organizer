9d0ee0ff648a7364aa17255e416815932ffd7e53
ThozamileMad <118363197+ThozamileMad@users.noreply.github.com>
2024-06-29 21:52:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from pprint import pprint
 
 file_extensions = {
     "Pictures": [
@@ -18,46 +17,46 @@ file_extensions = {
     ]
 }
 
-target_path = input("Please Enter The Path of The Target Directory: ")
-modified_target_path = Path(target_path)
-
-# List of Items In Directory
-target_dir = os.listdir(modified_target_path)
-
-# Getting Documents, Videos, Pictures, Music in Users Directory
-user_path = f"C:/Users/"
-user_items = os.listdir(Path(user_path))
+des_paths = {des_name: os.path.expanduser("~") + "\\" + des_name for des_name in file_extensions}
 
 
-def get_des_paths():
-    # Identifies the user's username then obtains destination paths
-    for dir in user_items:
-        dir_with_onedrive = Path(f"{user_path}{dir}/OneDrive")
-        if os.path.isdir(dir_with_onedrive):
-            user_name = f"{user_path}{dir}/"
-            destination_names = ["Documents", "Videos", "Pictures", "Music"]
-            destination_paths = [user_name + des_name for des_name in destination_names]
+def get_src_and_des_path(extension_lst, des_path):
+    target_dir = os.listdir(src_path)
+    paths = []
 
-            return destination_paths
+    for file_path in target_dir:
+        for extension in extension_lst:
+            # Compares files to extension
+            if extension in file_path:
+                path_item = {"src_path": src_path + "\\" + file_path,
+                             "des_path": des_path + "\\" + file_path,
+                             "des_dir": des_path}
 
-
-def move_files_to_des(destination_paths):
-    # Loops through files in targeted directory
-    for filename in target_dir:
-        for k, v in file_extensions.items():
-
-            # Identifies source and destination paths
-            for ext in v:
-                if ext in filename:
-                    for des_path in destination_paths:
-                        src_path = Path(target_path + "/" + filename)
-                        if k in des_path:
-                            des_path = Path(des_path + "/" + filename)
-
-                            # Moves file to appropriate Destination (e.g .mp3 to Music Directory)
-                            os.rename(src_path, des_path)
+                if path_item not in paths:
+                    paths.append(path_item)
+    return paths
 
 
-destination_paths = get_des_paths()
-move_files_to_des(destination_paths)
-print("Your Files have been successfully arranged")
+def move_files_based_on_type(src_des_dict):
+    for paths in src_des_dict:
+        s_path = paths["src_path"]
+        d_path = paths["des_path"]
+        dir_path = paths['des_dir']
+        os.rename(Path(s_path), Path(d_path))
+        print(f"File: '{s_path}', moved to: '{dir_path}'")
+
+
+def move_all_files():
+    path_data = [get_src_and_des_path(file_extensions[key], des_paths[key]) for key in file_extensions]
+    for lst in path_data:
+        move_files_based_on_type(lst)
+    print("All FILES REARRANGED")
+
+
+os.chdir("/")
+print(os.path.isdir('C:\\Users\\Church\\Documents\\Assignment   2024 INS511 -QUESTION.docx'))
+src_path = input("Please Enter The Path of The Target Directory: ")
+# Moves every file to it's appropriate folders (.mp3 to Music, .jpg to Pictures)
+move_all_files()
+
+# C:\Users\Church\Downloads
